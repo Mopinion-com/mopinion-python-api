a520abe2cf2e0b18b6b77cb68dfe19504b920214
kurtvooys <kurt.vooys@gmail.com>
2020-06-17 11:07:41 +0200

    Added API call functionality, consistency of str encodes, more tests!

--- mopinion/client.py
+++ mopinion/client.py
@@ -6,15 +6,41 @@ For more information, see: https://developer.mopinion.com/api/
 from base64 import b64encode
 import requests
 import hashlib
+import hmac
 
-BASE_URL = "https://api.mopinion.com"
 
-def get_signature_token(public_key, private_key):
-    session = requests.Session()
-    # The authorization method is public_key:private_key encoded as b64 string
-    auth_header = b64encode(f"{public_key}:{private_key}".encode()).decode()
-    headers = {"Authorization": "Basic " + auth_header}
-    r = session.get(BASE_URL + "/token", headers=headers)
-    # r.raise_for_status()
-    print(r.status_code)
-    return r.json()
+class Client:
+
+
+    def __init__(self, public_key, private_key):
+        self.BASE_URL = "https://api.mopinion.com"
+        self.public_key = public_key
+        self.private_key = private_key
+        self.signature_token = self.get_signature_token(public_key, private_key)
+
+
+    def get_signature_token(self, public_key, private_key):
+        # The authorization method is public_key:private_key encoded as b64 string
+        auth_header = b64encode(f"{public_key}:{private_key}".encode()).decode()
+        headers = {"Authorization": "Basic " + auth_header}
+        r = requests.get(self.BASE_URL + "/token", headers=headers)
+        r.raise_for_status()
+        return r.json()["token"]
+
+
+    def api_request(self, endpoint="/account", method="GET", body="", query=""):
+        uri_and_body = '{}|{}'.format(endpoint, body).encode('utf-8')
+        uri_and_body_hmac_sha256 = hmac.new(self.signature_token.encode('utf-8'), msg=uri_and_body, digestmod=hashlib.sha256).hexdigest()
+    
+        xtoken = b64encode('{}:{}'.format(self.public_key, uri_and_body_hmac_sha256).encode('utf-8'))
+        url = '{}{}{}'.format(self.BASE_URL, endpoint, query)
+        headers = {
+            'X-Auth-Token': xtoken,
+            'version':'1.18.14',
+            'verbosity':'full'
+        }
+        
+        response = requests.request(method, url, data=body, headers=headers, params=query)
+        response.raise_for_status()
+        return response, xtoken
+
--- test/test_api_request.py
+++ test/test_api_request.py
@@ -2,16 +2,17 @@ import mopinion.client as APIClient
 import unittest
 import json
 
-class AuthTest(unittest.TestCase):
+class APITest(unittest.TestCase):
 
-    def test_signature_token(self):
+    def test_api_request(self):
         with open("creds.json") as file:
             creds = json.load(file)
             public_key, private_key = creds["public-key"], creds["private-key"]
 
         client = APIClient.Client(public_key, private_key)
-        token = client.get_signature_token(public_key, private_key)
-        print(token)
+        response, xtoken = client.api_request()
+        assert response.json()["_meta"]["code"] == 200
+        print(response, xtoken)
 
 if __name__ == '__main__':
     unittest.main()
