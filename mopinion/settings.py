dcfb1effaff8f849680d92dcfa543013a3fd29ca
Jose <josevazjim88@gmail.com>
2022-02-23 09:11:29 +0100

    Adding...

--- mopinion/settings.py
+++ mopinion/settings.py
@@ -6,5 +6,5 @@ LATEST_VERSION = "2.0.0"
 # Some settings for dataclasses
 VERBOSITY_LEVELS = ["quiet", "normal", "full"]
 ITERATE_VERBOSITY_LEVELS = ["normal", "full"]
-VERSIONS = ["1.18.14", "2.0.0"]
+VERSIONS = ["1.18.14", "2.0.0", "2.1.0", "2.2.0"]
 CONTENT_NEGOTIATIONS = ["application/json", "application/x-yaml"]
